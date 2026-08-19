240bf73307b3108414840c374b959b1d36218874
Urfahamid <utkarshg228@gamil.com>
2026-08-19 10:20:38 +0530

    1st

---
+++ basic.js
@@ -0,0 +1,30 @@
+//var let const
+var name="vikas";
+name= "amit";
+console.log(name);
+var name="rahul";
+
+console.log(name);
+
+//let 
+let age=23;
+console.log(age);
+age=26;
+console.log(age);
+age=30;
+console.log(age);
+// const
+const studentName="amit";
+console.log(studentName);
+const student={
+}
+student.name="amit";
+student.city="noida";
+console.log(student);
+
+const arr=[1,2,3];
+const fruits=["apple","banana"];
+arr.push(4);
+fruits.push("mango");
+console.log(arr);
+console.log(fruits);
