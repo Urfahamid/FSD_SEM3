21b01c56cd803b33b205745fe352e8efc895d0b7
Urfahamid <utkarshg228@gamil.com>
2026-09-02 09:45:13 +0530

    commit

--- exception.js
+++ exception.js
@@ -1,10 +1,24 @@
 //exception handling
 //handling error
+// try {
+//     let a = 10;
+//     let b = 0;      
+//     let c = a/b;
+//     console.log(c);
+// } catch (error) {
+//     console.log("Error occurred:", error.message);
+// }
+//custom error
+function checkAge(age) {
+    if(age<18) {
+        throw new Error("not");
+
+    }
+    console.log("yes");
+
+}
 try {
-    let a = 10;
-    let b = 0;      
-    let c = a/b;
-    console.log(c);
+    checkAge(15);
 } catch (error) {
     console.log("Error occurred:", error.message);
 }
